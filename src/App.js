4d775ea305c2be1c35071d153f721700c8546411
sakdipong6529 <sakdipong6529@gmail.com>
2020-04-04 13:08:03 +0700

    config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import config from './config'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp(config);
+const auth = firebase.auth
+const provider = new firebase.auth.FacebookAuthProvider();
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {user:null}
+  } 
+  login = () => {
+    var that = this;
+    const result = auth().signInWithPopup(provider).then(function(result) {
+      var user = result.user;
+      console.log(user);
+      that.setState({user: user});
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  }
+  logout = () => {
+    var that = this;
+    auth().signOut().then(function() {
+      that.setState({user: null});
+    }).catch(function(error) {
+    });
+  }
+  renderName = () => {
+    const {user} = this.state
+    if(user)
+      return (<div><img src={`${user.photoURL}`} />{user ? `Hi, ${user.displayName}!` : 'Hi!'}</div>)
+  }
+  render() {
+    return (
+      <div className="App">
+          {this.renderName()}
+          <button onClick={this.login}>
+            Login with Facebook
+          </button>
+          <button onClick={this.logout}>
+            Logout
+          </button>
+      </div>
+    );
+  }
 }
-
 export default App;
