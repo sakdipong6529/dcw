1f0970b1ad977fbfd66516aa43326ce73b01b5fd
sakdipong6529 <sakdipong6529@gmail.com>
2020-04-08 16:32:17 +0700

    commit

--- Back-end.js
+++ Back-end.js
@@ -1,1 +1,55 @@
-....
+let express = require('express');
+let bodyParser = require('body-parser');
+let router = express.Router();
+let cors = require('cors');
+let app = express();
+app.use(cors());
+
+// all of our routes will be prefixed with /api
+app.use('/api', bodyParser.json(), router);   //[use json]
+app.use('/api', bodyParser.urlencoded({ extended: false }), router);
+
+let products = [  { 'no': 1, 'id': "5635512000", 'name': "Alizz" ,'surname':"Qii" ,'Major':"CoE", 'GPA':3.32},
+                  { 'no': 2, 'id': "5635512036", 'name': "Sakdipong",'surname':"Jaema"  ,'Major':"CoE",'GPA':2.50}
+               ];
+router.route('/home')
+    // get all products
+    .get((req, res) => res.json(products))
+    // insert a new product
+    .post((req, res) => {
+    var product = {};
+    product.no = products.length > 0 ? products[products.length - 1].no + 1 : 0;
+    product.name = req.body.name
+    product.surname = req.body.surname
+    product.id = req.body.id
+    product.Major = req.body.Major
+    product.GPA = req.body.GPA
+    products.push(product);
+    res.json({ message: 'products created!' })
+    })
+router.route('/products/:product_no')
+    .get((req, res) => {
+        let no = req.params.product_no
+        let index = products.findIndex(product => (product.no === +no))
+        res.json(products[index])                   // get a product
+    })
+    .put((req, res) => {                               // Update a bear
+        let no = req.params.product_no
+        let index = products.findIndex(product => (product.no === +no))
+        products[index].name = req.body.name;
+        products[index].surname = req.body.surname;
+        products[index].id = req.body.id;
+        products[index].Major = req.body.Major;
+        products[index].GPA = req.body.GPA;
+        res.json({ message: 'product updated!' + req.params.product_no });
+    })
+    .delete((req, res) => {                   // Delete a bear
+        let no = req.params.product_no
+        let index = products.findIndex(product => product.no === +no)
+        products.splice(index, 1)
+        res.json({ message: 'product deleted: ' + req.params.product_no });
+    })
+
+
+app.use("*", (req, res) => res.status(404).send('404 Not found'));
+app.listen(80, () => console.log("Server is running"));
--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
-import config from './config'
+import React from 'react';
+import './App.css'
+import axios from 'axios'
+import NavBar from './components/NavBar'
 
-firebase.initializeApp(config);
-const auth = firebase.auth
-const provider = new firebase.auth.FacebookAuthProvider();
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {user:null}
-  } 
-  login = () => {
-    var that = this;
-    const result = auth().signInWithPopup(provider).then(function(result) {
-      var user = result.user;
-      console.log(user);
-      that.setState({user: user});
-    }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
+function App() {
+  const dispatch = useDispatch();
+  
+  const getStudents = async () => {
+    const result = await axios.get(`http://localhost/api/home`)
+    const action = {type:'GET_STUDENTS',student: result.data}
+    dispatch(action)
   }
-  logout = () => {
-    var that = this;
-    auth().signOut().then(function() {
-      that.setState({user: null});
-    }).catch(function(error) {
-    });
-  }
-  renderName = () => {
-    const {user} = this.state
-    if(user)
-      return (<div><img src={`${user.photoURL}`} />{user ? `Hi, ${user.displayName}!` : 'Hi!'}</div>)
-  }
-  render() {
-    return (
-      <div className="App">
-          {this.renderName()}
-          <button onClick={this.login}>
-            Login with Facebook
-          </button>
-          <button onClick={this.logout}>
-            Logout
-          </button>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+
+      <h2>Redux student</h2>
+      <StudentList/>
+      <InputForm/>
+      
+    </div>
+  );
 }
+
 export default App;
